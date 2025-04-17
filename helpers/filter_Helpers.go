2e2b16476ee4614221c52b8db9dc053d3f94a72b
ahmed <ahmed@baid.com>
2025-04-17 13:16:05 +0100

    last version

--- helpers/filter_Helpers.go
+++ helpers/filter_Helpers.go
@@ -7,24 +7,24 @@ import (
 	"groupie/tools"
 )
 
-func CheckCreationDate(a *tools.Artists, min string, max string) bool {
-	minV, _ := strconv.Atoi(min)
-	maxV, _ := strconv.Atoi(max)
-	if (minV == 1987 && maxV == 1987) || (a.CreationDate >= minV && maxV >= a.CreationDate) {
+func CheckCreationDate(allArtists *tools.Artists, minCr string, maxCr string) bool {
+	minCrDate, _ := strconv.Atoi(minCr)
+	maxCrDate, _ := strconv.Atoi(maxCr)
+	if (minCrDate == 1987 && maxCrDate == 1987) || (allArtists.CreationDate >= minCrDate && maxCrDate >= allArtists.CreationDate) {
 		return true
 	}
 	return false
 }
 
-func CheckFirstAlbum(a *tools.Artists, year1, year2 string) bool {
+func CheckFirstAlbum(allArtists *tools.Artists, year1, year2 string) bool {
 	if len(year1) == 0 && len(year2) == 0 {
 		return true
 	}
 
-	minyear, _ := strconv.Atoi(year1)
-	maxyear, _ := strconv.Atoi(year2)
-	firstAlbYear := strings.Split(a.FirstAlbum, "-")[2]
-	for i := minyear; i <= maxyear; i++ {
+	minYear, _ := strconv.Atoi(year1)
+	maxYear, _ := strconv.Atoi(year2)
+	firstAlbYear := strings.Split(allArtists.FirstAlbum, "-")[2]
+	for i := minYear; i <= maxYear; i++ {
 		if firstAlbYear == strconv.Itoa(i) {
 			return true
 		}
@@ -32,14 +32,14 @@ func CheckFirstAlbum(a *tools.Artists, year1, year2 string) bool {
 	return false
 }
 
-func CheckNumberOfMembers(a *tools.Artists, members []string) bool {
+func CheckNumberOfMembers(allArtists *tools.Artists, members []string) bool {
 	if len(members) == 0 {
 		return true
 	}
 
 	for _, e := range members {
 		nb, _ := strconv.Atoi(e)
-		if len(a.Members) == nb {
+		if len(allArtists.Members) == nb {
 			return true
 		}
 	}
