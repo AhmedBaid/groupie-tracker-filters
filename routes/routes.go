d10db478ec0353910f541aeb421270502147e964
AhmedBaid <150438476+AhmedBaid@users.noreply.github.com>
2025-04-29 16:53:56 +0000

    last version

---
+++ routes/routes.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	"net/http"
+
+	"groupie/handler"
+)
+
+func Route() {
+	// our handlers
+	http.HandleFunc("/", handler.Groupie_Func)
+	http.HandleFunc("/static/", handler.Style_Func)
+	http.HandleFunc("/details", handler.Detail_Card_Func)
+	http.HandleFunc("/Filter", handler.FilterHandler)
+}
